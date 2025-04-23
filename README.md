c7650930887a88aa5ce601b2cd07c1f742fd73e8
Brunoaichinger <brunoaichinger@gmail.com>
2025-04-23 18:10:20 -0300

    Atualização do README.md aula 03

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# aula03
- 
+# Aula 03 – Tipos Primitivos e Formatação
+
+Este projeto faz parte da Trilha de Estudos em Java.
+
+## 🧠 Conceitos Trabalhados
+- Tipos primitivos em Java:
+    - `int`, `double`, `char`, `boolean`
+- Entrada de dados com `Scanner`
+- Limpeza de buffer após leitura de números
+- Exibição formatada com `System.out.printf`
+
+## 📌 Exercício proposto
+Peça os seguintes dados ao usuário:
+- Nome (String)
+- Idade (int)
+- Altura (double)
+- Sexo (char)
+
+E exiba uma mensagem formatada com todos os dados.
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+    Scanner entrada = new Scanner (System.in);
 
-        
+
+        System.out.println("Digite o seu nome completo");
+        String nome = entrada.nextLine();
+        System.out.println("Digite a sua idade:");
+        int idade = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Digite a sua altura:");
+        double altura = entrada.nextDouble();
+        entrada.nextLine();
+        System.out.println("Digite o seu sexo(M/F):");
+        char sexo = entrada.next().charAt(0);
+
+        System.out.printf("Nome:%s\nIdade:%d\nAltura:%.2f\nSexo:%s", nome, idade, altura, sexo);
+
+
+
     }
 }
